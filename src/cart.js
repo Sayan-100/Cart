27b9268aa9e21e6b0d2a0719de7f74dbca99ec2e
Sayan Biswas <bsayan289@gmail.com>
2022-04-30 18:57:15 +0530

    delete product from cart

--- src/cart.js
+++ src/cart.js
@@ -38,6 +38,48 @@ class Cart extends React.Component {
         //multiple getas it messier
         // this.testing();
     }
+
+    handleIncreaseQuantity = (product) => {
+
+        console.log('Hey please increse the quantity of', product);
+        const {products}=this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty = products[index].qty + 1;
+
+        this.setState({
+            products : products
+        })
+    }
+
+    handleDecreaseQuantity = (product) => {
+        console.log('Hey please increse the quantity of', product);
+        const {products}=this.state;
+        const index = products.indexOf(product);
+
+        if(products[index].qty === 0)
+        {
+            return;
+        }
+
+        products[index].qty = products[index].qty - 1;
+
+
+        this.setState({
+            products : products
+        })
+    }
+
+    handleDeleteProduct = (id) => {
+        const {products} = this.state;
+        
+        const items = products.filter((item) => item.id !== id); //[{id not equall to argument id}]
+
+        this.setState({
+            products : items
+        })
+    }
+
     render()
     {
         // const arr = [1, 2, 3, 4, 5];
@@ -58,6 +100,9 @@ class Cart extends React.Component {
                          <CartItem 
                            product={product} 
                            key ={product.id}
+                           onIncreaseQuantity={this.handleIncreaseQuantity}
+                           onDecreaseQuantity={this.handleDecreaseQuantity}
+                           onDeleteProduct={this.handleDeleteProduct}
                          />
                         ) 
                     })
--- src/cartitem.js
+++ src/cartitem.js
@@ -30,90 +30,90 @@ class CartItem extends React.Component {
     //   });
     // }
 
-    increaseQuantity = () => {
-        //this.state.qty += 1;
-        // console.log('this', this.state);
-        //setState 1
+    // increaseQuantity = () => {
+    //     //this.state.qty += 1;
+    //     // console.log('this', this.state);
+    //     //setState 1
 
-        //Batch object form
-        // this.setState({
-        //   qty : this.state.qty + 1
-        // }, () => {
+    //     //Batch object form
+    //     // this.setState({
+    //     //   qty : this.state.qty + 1
+    //     // }, () => {
 
-        //});
+    //     //});
 
-        // this.setState({
-        //   qty : this.state.qty + 1
-        // });
+    //     // this.setState({
+    //     //   qty : this.state.qty + 1
+    //     // });
 
-        // this.setState({
-        //   qty : this.state.qty + 5
-        // });//last call 
+    //     // this.setState({
+    //     //   qty : this.state.qty + 5
+    //     // });//last call 
 
-        //calling it three times but it is changing by single
-        //Batching -- call is merged to single state call
+    //     //calling it three times but it is changing by single
+    //     //Batching -- call is merged to single state call
 
 
-        //setState 2
+    //     //setState 2
          
-        //if previous state required use this
-        // this.setState((prevState) => {
-        //   return {
-        //     qty : prevState.qty + 1
-        //   }
+    //     //if previous state required use this
+    //     // this.setState((prevState) => {
+    //     //   return {
+    //     //     qty : prevState.qty + 1
+    //     //   }
           
-        // });
+    //     // });
 
-        //if previous state required use this
-        // this.setState((prevState) => {
-        //   return {
-        //     qty : prevState.qty + 1
-        //   }
-        // });
+    //     //if previous state required use this
+    //     // this.setState((prevState) => {
+    //     //   return {
+    //     //     qty : prevState.qty + 1
+    //     //   }
+    //     // });
         
-        // //if previous state required use this
-        // this.setState((prevState) => {
-        //   return {
-        //     qty : prevState.qty + 1
-        //   }
-        // });
+    //     // //if previous state required use this
+    //     // this.setState((prevState) => {
+    //     //   return {
+    //     //     qty : prevState.qty + 1
+    //     //   }
+    //     // });
 
-        // maintains a queue previous state updated
-        // callback 1
-        // callback 2
-        // callback 3
+    //     // maintains a queue previous state updated
+    //     // callback 1
+    //     // callback 2
+    //     // callback 3
 
-        this.setState((prevState) => {
-          return {
-            qty : prevState.qty + 1
-          }
-        }, () => {
-          console.log('this.state', this.state);
-        });
+    //     this.setState((prevState) => {
+    //       return {
+    //         qty : prevState.qty + 1
+    //       }
+    //     }, () => {
+    //       console.log('this.state', this.state);
+    //     });
 
-        // console.log('this state', this.state);
-
-
+    //     // console.log('this state', this.state);
 
 
 
-    }
 
-    decreaseQuantity = () => {
-      console.log('this', this.state);
 
-      const {qty} = this.state;
-      if(qty == 0)
-      {
-        return;
-      }
+    // }
 
-      this.setState((prevState) => {
-        return {
-          qty : prevState.qty - 1
-        }
-      })
-    }
+    // decreaseQuantity = () => {
+    //   console.log('this', this.state);
+
+    //   const {qty} = this.state;
+    //   if(qty == 0)
+    //   {
+    //     return;
+    //   }
+
+    //   this.setState((prevState) => {
+    //     return {
+    //       qty : prevState.qty - 1
+    //     }
+    //   })
+    // }
 
     //arrow function will add the value to the instance of the CartItem
 
@@ -122,6 +122,11 @@ class CartItem extends React.Component {
       console.log('this props', this.props);
         // const {price, title, qty} = this.state;
         const {price, title, qty} = this.props.product;
+        const {
+          product, 
+          onIncreaseQuantity, 
+          onDecreaseQuantity, 
+          onDeleteProduct} = this.props;
         return(
             <div className  = "cart-item">
               {this.props.jsx}
@@ -140,7 +145,7 @@ class CartItem extends React.Component {
                       alt="increase" 
                       className ="action-icons" 
                       src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
-                      onClick = {this.increaseQuantity}
+                      onClick={() => onIncreaseQuantity(product)}
                     //   onClick={this.increaseQuantity.bind(this)}
                     
                       />
@@ -148,13 +153,13 @@ class CartItem extends React.Component {
                       alt="decrease" 
                       className ="action-icons" 
                       src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
-                      onClick={this.decreaseQuantity}
+                      onClick={() => onDecreaseQuantity(product)}
                       />
                     <img 
                       alt="delete" 
                       className ="action-icons" 
                       src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
-                    //   onClick={this}
+                      onClick={() => onDeleteProduct(product.id)}
                       />
                 </div>
                 </div>
